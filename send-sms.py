f51fd8c83503f975c5419df01d3315aabcce570e
Darius <78953908+DariusVPrice@users.noreply.github.com>
2025-05-06 15:40:11 -0400

    Update send-sms.py

--- send-sms.py
+++ send-sms.py
@@ -2,7 +2,9 @@ from main import *
 
 # sinch send
 sinch_client = SinchClient(
-key_id="9398bec7-4b6d-4bd0-8f41-dc7b1924d5e9",
-key_secret="8KxutGfZw-CLfAu3e13Zg~RqaS",
-project_id="9b3fd919-2fee-4365-86f2-c7864451810d"
+key_id="",
+key_secret="",
+project_id=""
 )
+
+# ID's removed
