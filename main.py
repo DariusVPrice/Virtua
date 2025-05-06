6ddbfce5eaab3cbf01e011b11441e4005fbe321c
Darius <78953908+DariusVPrice@users.noreply.github.com>
2025-05-06 15:39:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from sinch import SinchClient
 from flask import Flask, request
 
 app = Flask(__name__)
-# DGPW4EPS4EUJQ7UNRWH6BSAU
+
 
 # sinch send
 sinch_client = SinchClient(
-key_id="9398bec7-4b6d-4bd0-8f41-dc7b1924d5e9",
-key_secret="8KxutGfZw-CLfAu3e13Zg~RqaS",
-project_id="9b3fd919-2fee-4365-86f2-c7864451810d"
+key_id="",
+key_secret="",
+project_id=""
 )
 
-client = OpenAI(api_key=os.environ.get("OPEN_API_KEY","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"))
+client = OpenAI(api_key=os.environ.get(""))
 
 #Local User data
 current_time = time.ctime()
@@ -32,12 +32,12 @@ def result():
     inbound_message = request.get_json()
     print(inbound_message)
 
-    if all(key in inbound_message for key in ["body", "12166003244", "12085815118"]):
+    if all(key in inbound_message for key in ["body", "", ""]):
         sinch_client.sms.batches.send(
             body="Thank you for using the Sinch SDK. You sent: " + inbound_message["body"],
             delivery_report="none",
-            to=[inbound_message["12085815118"]],
-            from_=inbound_message["12166003244"]
+            to=[inbound_message[""]],
+            from_=inbound_message[""]
         )
 
         return "Inbound message received", 200
@@ -62,8 +62,8 @@ print(response.choices[0].message.content)
 #Send code
 send_batch_response = sinch_client.sms.batches.send(
     body= response.choices[0].message.content,
-    to=["12166003244"],
-    from_="12085815118",
+    to=[""],
+    from_="",
     delivery_report="none"
 )
 #print(send_batch_response)
